04d5315e6b401d18e4c0d7857c7b3321fb58fe9f
Pradeep Chauhan <pradeepchauhan91@gmail.com>
2022-06-17 18:11:28 +0530

    trying with babel jest

--- jest.config.mjs
+++ jest.config.mjs
@@ -165,7 +165,9 @@ export default {
 
   // A map from regular expressions to paths to transformers
   // transform: undefined,
-  transform: {},
+  transform: {
+    '^.+\\.[t|j]sx?$': 'babel-jest',
+  },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
